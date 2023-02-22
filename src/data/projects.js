3b7dc472236d47968ab3e184ac1ec1ff7a4fa3f5
Ekbal41 <mdasifekbalshagor@gmail.com>
2023-02-22 23:24:16 +0600

    animation added

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ const projects = [
         id: 1,
         title: "Pizza",
         description: "A pizza is a pizza that is made of cheese and toppings. It is a type of food that is made of the ingredients of the cheese and the toppings.",
-        image: "src/assets/vue3.jpeg",
+        image: "vue3.jpeg",
         github: "https://www.facebook.com/",
         link:"https://www.facebook.com/",
     },
@@ -11,7 +11,7 @@ const projects = [
         id: 2,
         title: "Pizza",
         description: "A pizza is a pizza that is made of cheese and toppings. It is a type of food that is made of the ingredients of the cheese and the toppings.",
-        image:  "src/assets/vue3.jpeg",
+        image:  "vue3.jpeg",
         github: "#",
         link: "#"
     },
@@ -19,7 +19,7 @@ const projects = [
         id: 3,
         title: "Pizza",
         description: "A pizza is a pizza that is made of cheese and toppings. It is a type of food that is made of the ingredients of the cheese and the toppings.",
-        image:  "src/assets/vue3.jpeg",
+        image:  "vue3.jpeg",
         github: "",
         link: "#"
     },
